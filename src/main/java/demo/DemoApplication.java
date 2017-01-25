cf735ce7530110b7ef25c40f0a252ea96509aaf3
Ajay Koranne <akoranne@gmail.com>
2017-01-25 15:19:19 -0600

    checkin

---
+++ src/main/java/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "Hello world";
+	}
+
+	@RequestMapping(value = "/kill")
+	public void kill() {
+		System.exit(1);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
