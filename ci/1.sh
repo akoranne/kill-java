0b470228d5801483707d8e077253001266f2bf79
Ajay Koranne <akoranne@gmail.com>
2017-01-26 10:16:54 -0600

    ignore creds

--- ci/1.sh
+++ ci/1.sh
@@ -1,3 +1,3 @@
-fly -t local destroy-pipeline -p kill-java
+#	fly -t local destroy-pipeline -p kill-java
 fly -t local set-pipeline -p kill-java -c pipeline.yml -l credentials-ecslab.yml
 fly -t local unpause-pipeline --pipeline kill-java
